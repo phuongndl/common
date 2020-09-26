19244cdc52196bdeaf527e704df0869c4da1e7bf
Thang Nguyen <k.mendo87@gmail.com>
2020-09-27 00:47:42 +0700

    Update common

--- src/main/java/com/microservice/payload/JwtResponse.java
+++ src/main/java/com/microservice/payload/JwtResponse.java
@@ -12,9 +12,9 @@ public class JwtResponse {
 	private String username;
 	private String email;
 	private List<String> roles;
-	private String[] permissions;
+	private List<String> permissions;
 
-	public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles, String[] permissions) {
+	public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles, List<String> permissions) {
 		this.token = accessToken;
 		this.id = id;
 		this.username = username;
